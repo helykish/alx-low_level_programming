e50e1cf6f96da225c293e933d6921e59fc4d7e71
helykish <111018650+helykish@users.noreply.github.com>
2023-07-15 02:00:33 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - main block
+ * main - The main block
  * Description: Print all numbers of base 10, starting from 0.
- * You are not allowed to use any variable of type char.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` twice.
+ * Don't  use any variable of type char.
+ * Use only `putchar` to print to console.
+ * Use only `putchar` twice.
  * Return: 0
  */
 
