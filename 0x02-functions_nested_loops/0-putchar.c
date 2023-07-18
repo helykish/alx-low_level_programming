9fc708631f6589c9de91d464b2f47fc75de19048
helykish <111018650+helykish@users.noreply.github.com>
2023-07-19 02:52:22 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,5 @@
 #include "main.h"
 
-/**
- * main - Print "_putchar" followed by a new line.
- * Description: You are not allowed to include standard libraries.
- * Return: 0
- */
-
 int main(void)
 {
 	char myVar[] = "_putchar";
