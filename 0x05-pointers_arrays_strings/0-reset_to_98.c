c795e9d2c8c64d77a167e724e63a95c77a7287b1
helykish <111018650+helykish@users.noreply.github.com>
2023-07-24 23:38:41 +0300

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of n to 98
+ * reset_to_98 - updating value of n to 98
  * @n: integer pointer to a variable to be updated
- * Return: nothing
+ * Return: return nothing
  */
 
 void reset_to_98(int *n)
