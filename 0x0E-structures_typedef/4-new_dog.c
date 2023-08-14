f9dc335edc26355ba18282bd3836c0480e9baafd
helykish <111018650+helykish@users.noreply.github.com>
2023-08-15 00:03:32 +0300

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,10 +2,10 @@
 #include "dog.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
+ * _strlen - print length of a string
+ * @s: evaluate the strng
  *
- * Return: the length of the string
+ * Return: string length
  */
 int _strlen(char *s)
 {
@@ -22,11 +22,11 @@ int _strlen(char *s)
 }
 
 /**
- * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
+ * *_strcpy - the strmg pointed by stc
+ * including the null byte (\0)
+ * buffer pointed to by dest
+ * @dest: pointer to the buffer copyed the string
+ * @src: copied string
  *
  * Return: the pointer to dest
  */
@@ -51,10 +51,10 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * new_dog - printf a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
  *
  * Return: pointer to the new dog (Success), NULL otherwise
  */
