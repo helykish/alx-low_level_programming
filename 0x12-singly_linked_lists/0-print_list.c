7b729063d2e813dfea2957b6c047dfc7b3d76f8b
helykish <111018650+helykish@users.noreply.github.com>
2023-08-24 21:07:07 +0300

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * print_list - prints all the elements of a list_t list.
+ * print_list - all elements of a list_t list.
  * @h: singly linked list.
- * Return: number of elements in the list.
+ * Return: elements number in the list.
  */
 
 size_t print_list(const list_t *h)
