3348f3727c96124663de505d5bb7545ee0ea1405
helykish <111018650+helykish@users.noreply.github.com>
2023-08-29 20:45:32 +0300

    Task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * listint_len - counts the number of nodes in a linked list
+ * listint_len - counts the nodes in a linked list
  * @h: head of the list
  *
- * Return: the number of elements
+ * Return: the no of elements
  */
 size_t listint_len(const listint_t *h)
 {
