6f22bc8d296b997d13e91bd6d20e897a064ef6ba
helykish <111018650+helykish@users.noreply.github.com>
2023-09-01 00:05:10 +0300

    Task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * set_bit - sets the value of a bit to 1 at a given index.
- * @n: number to set
+ * set_bit - func set value of a bit to 1 at a given index.
+ * @n: no. to set
  * @index: index at which to set bit
  *
- * Return: 1 if it worked, or -1 if an error occurred
+ * Return: 1 if successful, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
