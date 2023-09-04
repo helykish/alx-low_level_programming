7a9fcdf8b3ac36e2a0b64bc728f4f39c70c0c455
helykish <111018650+helykish@users.noreply.github.com>
2023-09-05 00:09:20 +0300

    Task  1 solution

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
+ * create_file - function to create a file
  * @filename: filename.
- * @text_content: content writed in the file.
+ * @text_content: content wried in files.
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: always 1 if it success else -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
