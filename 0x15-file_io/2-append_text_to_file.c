918592713fb74ee22b53f9b732b0c20d014b615e
helykish <111018650+helykish@users.noreply.github.com>
2023-09-05 00:16:56 +0300

    Task 3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
+ * append_text_to_file - func that appends text at end of a file
  * @filename: filename.
  * @text_content: added content.
  *
- * Return: 1 if the file exists. -1 if the fails does not exist
+ * Return: always 1 if the file exists. -1 if the fails does not exist
  * or if it fails.
  */
 int append_text_to_file(const char *filename, char *text_content)
