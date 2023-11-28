591a3738e7001e8fe83e868da6aef6d4391ce3f1
helykish <111018650+helykish@users.noreply.github.com>
2023-11-28 04:08:58 +0300

    100-elf_header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -18,8 +18,8 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
- * check_elf - Checks if a file is an ELF file.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
+ * check_elf - func checks if a file is an ELF file.
+ * @e_ident: A pointer 2 an array containing the ELF magic naba.
  *
  * Description: If the file is not an ELF file - exit code 98.
  */
@@ -41,7 +41,7 @@ void check_elf(unsigned char *e_ident)
 }
 
 /**
- * print_magic - Prints the magic numbers of an ELF header.
+ * print_magic - Prints the magic nabas of an ELF header.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
  * Description: Magic numbers are separated by spaces.
